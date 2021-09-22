2cc932932ab920f4eba3ce3b6c55dd2a4a6709cc
Erin Marshall <limarshall@wisc.edu>
2021-09-22 14:02:04 -0500

    env.sh for cs machines

---
+++ env.sh
@@ -0,0 +1,5 @@
+export MGC_HOME=/s/mentor-2018/@sys/V10.0BSXE/MGC_HOME.ixl
+export SDD_HOME=/s/mentor-2018/@sys/V10.0BSXE/SDD_HOME
+export MGLS_LICENSE_FILE=/s/mentor-2018/etc/cust/mgls/mgc.licenses
+export PATH=$PATH:/s/mentor-2018/@sys/bin:/s/mentor-2018/@sys/bin.pclinux:/s/mentor-2018/@sys/V10.0BSXE/MGC_HOME.ixl/bin:/s/mentor-2018/@sys/modelsim_dlx/bin
+export PATH=$PATH:/u/s/w/swamit/courses/cs552/fall2021/handouts/bins
