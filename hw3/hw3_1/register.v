658ce5f3ab6dea5d80bc181cfee2a8a1ab9b0db3
Erin Marshall <limarshall@wisc.edu>
2021-10-09 14:54:20 -0500

    clean up hw3_1

--- hw3/hw3_1/register.v
+++ hw3/hw3_1/register.v
@@ -1,14 +1,16 @@
-module register(
+module register
+#(parameter WIDTH=16)
+(
 	input  wire clk,
 	input  wire rst,
-	input  wire [15:0] write_data,
+	input  wire [WIDTH-1:0] write_data,
 	input  wire write_en,
-	output wire [15:0] read_data
+	output wire [WIDTH-1:0] read_data
 );
-	wire [15:0] d;
+	wire [WIDTH-1:0] d;
 	assign d = write_en ? write_data : read_data; // loopback q -> d unless we're writing
 
-	dff reg_flops [15:0] (
+	dff reg_flops [WIDTH-1:0] (
 		.clk(clk),
 		.rst(rst),
 		.q(read_data),
--- hw3/hw3_1/rf.v
+++ hw3/hw3_1/rf.v
@@ -19,9 +19,10 @@ module rf (
       .rst(rst),
       .write_data(writedata),
       .write_en(write_lines),
-      .read_data({reg_out_0, reg_out_1, reg_out_2, reg_out_3, reg_out_4, reg_out_5, reg_out_6, reg_out_7})
+      .read_data({reg_out_7, reg_out_6, reg_out_5, reg_out_4, reg_out_3, reg_out_2, reg_out_1, reg_out_0})
    );
 
+   // output select muxes
    always @* case (read1regsel)
       3'h0 : read1data = reg_out_0;
       3'h1 : read1data = reg_out_1;
@@ -43,12 +44,13 @@ module rf (
       3'h7 : read2data = reg_out_7;
    endcase
 
-   assign write_lines[7] = (writeregsel == 0) && write;
-   assign write_lines[6] = (writeregsel == 1) && write;
-   assign write_lines[5] = (writeregsel == 2) && write;
-   assign write_lines[4] = (writeregsel == 3) && write;
-   assign write_lines[3] = (writeregsel == 4) && write;
-   assign write_lines[2] = (writeregsel == 5) && write;
-   assign write_lines[1] = (writeregsel == 6) && write;
-   assign write_lines[0] = (writeregsel == 7) && write;
+   // demux logic to identify write outputs.
+   assign write_lines[0] = (writeregsel == 3'h0) && write;
+   assign write_lines[1] = (writeregsel == 3'h1) && write;
+   assign write_lines[2] = (writeregsel == 3'h2) && write;
+   assign write_lines[3] = (writeregsel == 3'h3) && write;
+   assign write_lines[4] = (writeregsel == 3'h4) && write;
+   assign write_lines[5] = (writeregsel == 3'h5) && write;
+   assign write_lines[6] = (writeregsel == 3'h6) && write;
+   assign write_lines[7] = (writeregsel == 3'h7) && write;
 endmodule
