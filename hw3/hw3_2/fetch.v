8822057091534316fc3f66e9f91004ac7c0d8dbf
Erin Marshall <limarshall@wisc.edu>
2021-10-09 14:54:44 -0500

    hw3_2

--- hw3/hw3_2/fetch.v
+++ hw3/hw3_2/fetch.v
@@ -1,10 +1,35 @@
-module fetch (clk, rst, err, instr);
+module fetch(
+    input  wire clk,
+    input  wire rst,
+    output wire err,
+    output wire [15:0] instr
+);
+    wire [15:0] pc;
+    wire [15:0] next_pc;
+    dff pc_flops [15:0] (
+        .clk(clk),
+        .rst(rst),
+        .d(next_pc),
+        .q(pc)
+    );
 
-	input clk;
-	input rst;
-	output err;
-	output [15:0] instr;
+    // assign next_pc = pc + 2;
+    adder16 pc_adder(
+        .A   (pc),
+        .B   (16'h2), // pc' = pc+2
+        .S   (next_pc),
+        .Cin (0),
+        .Cout()
+    );
 
-	// Your Code goes here
-
+    memory2c mem (
+        .clk       (clk),
+        .rst       (rst),
+        .wr        (0), // never need to write in this problem
+        .data_in   (0), // ibid
+        .createdump(0), // don't need to check memory at end of run
+        .addr      (pc),
+        .enable    (1), // enable memory so we can read!
+        .data_out  (instr)
+    );
 endmodule
