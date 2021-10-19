f20d947d24d84a15ab955c879d9bce9d868db590
Erin Marshall <limarshall@wisc.edu>
2021-10-19 14:02:49 -0500

    hw3_1: use old parameter syntax so name-convention-check is happy

--- hw3/hw3_1/register.v
+++ hw3/hw3_1/register.v
@@ -1,12 +1,11 @@
-module register
-#(parameter WIDTH=16)
-(
-	input  wire clk,
-	input  wire rst,
-	input  wire [WIDTH-1:0] write_data,
-	input  wire write_en,
-	output wire [WIDTH-1:0] read_data
-);
+module register(clk, rst, write_data, write_en, read_data);
+	parameter WIDTH = 16;
+	input  wire clk;
+	input  wire rst;
+	input  wire [WIDTH-1:0] write_data;
+	input  wire write_en;
+	output wire [WIDTH-1:0] read_data;
+
 	wire [WIDTH-1:0] d;
 	assign d = write_en ? write_data : read_data; // loopback q -> d unless we're writing
 
