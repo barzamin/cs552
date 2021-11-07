d78586872305bf673f2fb51f1a6b18ef364cf88f
Erin Marshall <limarshall@wisc.edu>
2021-11-07 13:04:13 -0600

    alu: tests passing <3

--- proj_unpipelined/alu.v
+++ proj_unpipelined/alu.v
@@ -5,12 +5,12 @@ module alu(
     output reg [15:0] out,
     output wire carryout
 );
+    `include "ops.vh"
+
     // so i drew out this very careful design and everything (except for screwing up bit-reversing: you need two reversers).
     // and i'm going to nearly throw it away in favor of heavily using case. this is because any reasonable synthesis tool
     // will produce a fast, small, but impenetrable cloud of reduced boolean logic rather than my explicit,
     // readable but slower muxing and gating.
-
-    assign A_shl8 = {A[7:0], 8'b0}; // A << 8. for SLBI
 
     // ---------------- add/sub unit ----------------
     reg sub;
@@ -31,9 +31,9 @@ module alu(
     reg rotate, prereverse, postreverse;
 
     reverser prereverser(
-        .in(A),
+        .in     (A),
         .reverse(prereverse),
-        .out(prereversed)
+        .out    (prereversed)
     );
 
     shifter shifter(
@@ -44,55 +44,69 @@ module alu(
     );
 
     reverser postreverser(
-        .in(A),
+        .in     (shifter_out),
         .reverse(postreverse),
-        .out(reverser_out)
+        .out    (reverser_out)
     );
 
     // you have no idea how badly i want to be able to use always_comb :(
     always @* begin
         // defaults: don't infer latches!
-        sub = 0;
+        sub = 1'b0;
         shamt = B[3:0];
+        rotate = 1'b0;
+        prereverse = 1'b0;
+        postreverse = 1'b0;
 
         casex (op)
-            4'b?000 : begin // XOR A ^ B
+            default : begin // passthru A
+                out = A;
+            end
+            ALU_XOR : begin // XOR A ^ B
                 out = A ^ B;
             end
-            4'b?100 : begin // (A << 8) | B. literally just used for SLBI
-                out = A_shl8 | B;
+            ALU_SLBI : begin // (A << 8) | B. literally just used for SLBI
+                out = {A[7:0], B[7:0]};
             end
-            4'b??01 : begin // ANDN A & ~B
+            ALU_ANDN : begin // ANDN A & ~B
                 out = A & ~B;
             end
-            4'b?010 : begin // A + B
+            ALU_ADD : begin // A + B
                 out = sum;
-                sub = 0;
+                sub = 1'b0;
             end
-            4'b0110 : begin // -A + B
+            ALU_SUB : begin // -A + B
                 out = sum;
-                sub = 1;
+                sub = 1'b1;
             end
-            4'b1110 : begin // BTR
+            ALU_BTR : begin // BTR
                 out = reverser_out;
                 prereverse = 0;
                 postreverse = 1;
                 rotate = 0;
                 shamt = 4'b0000;
             end
-            4'b0011 : begin // SRL (shift right logical)
+            ALU_SRL : begin // shift right logical
                 out = reverser_out;
             end
-            4'b1011 : begin // SLL (shift left logical)
+            ALU_SLL : begin // shift left logical
                 out = reverser_out;
+
+                prereverse = 1'b1;
+                postreverse = 1'b1;
             end
-            4'b0111 : begin // ROR (rotate right)
+            ALU_ROR : begin // rotate right
                 out = reverser_out;
+
+                rotate = 1'b1;
             end
-            4'b1111 : begin // ROL (rotate left)
+            ALU_ROL : begin // rotate left
                 out = reverser_out;
+
+                rotate = 1'b1;
+                prereverse = 1'b1;
+                postreverse = 1'b1;
             end
-            // default : /* default */;
         endcase
     end
 endmodule
---
+++ proj_unpipelined/tb_alu.v
@@ -0,0 +1,124 @@
+// lovingly stolen from hw2, and molded to fit my needs
+module tb_alu();
+    `include "ops.vh"
+
+    reg  [15:0] A;
+    reg  [15:0] B;
+    reg  [3:0]  op;
+    wire [15:0] out;
+    wire        carryout;
+
+    reg         fail;
+
+    reg        cerror;
+    reg [31:0] gold_out;
+    reg        gold_carryout;
+    integer idx;
+
+    // clockgen
+    wire clk, rst;
+    clkrst c0(
+        // Outputs
+        .clk(clk),
+        .rst(rst),
+        // Inputs
+        .err(1'b0)
+    );
+
+    alu dut (
+        .A(A),
+        .B(B),
+        .op(op),
+        .out(out),
+        .carryout(carryout)
+    );
+
+    initial begin
+        A = 16'b0000;
+        B = 16'b0000;
+        op = ALU_PASS;
+        fail = 0;
+
+    #5000;
+    if (fail)
+        $display("TEST FAILED");
+    else
+        $display("TEST PASSED"); $finish;
+    end
+
+    // generate test vectors
+    always @(posedge clk) begin
+        A  = $random;
+        B  = $random;
+        op = $random;
+    end
+
+    // compare result to golden
+    always @(negedge clk) begin
+        cerror = 1'b0;
+        gold_out = 32'h0000_0000;
+        gold_carryout = 1'b0;
+
+        case (op)
+            default : begin // ALU_PASS
+                gold_out = A;
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+
+            ALU_XOR : begin
+                gold_out = A ^ B;
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+            ALU_SLBI : begin
+                gold_out = (A << 8) | B[7:0];
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+            ALU_ANDN : begin
+                gold_out = A & ~B;
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+
+            ALU_ADD : begin
+                gold_out = A + B;
+                if (gold_out[16]) gold_carryout = 1'b1; // carried out :)
+
+                if ((gold_out[15:0] !== out) || (gold_carryout !== carryout)) cerror = 1'b1;
+            end
+            ALU_SUB : begin
+                gold_out = B - A;
+
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+
+            ALU_BTR : begin
+                gold_out = {A[0], A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[8], A[9], A[10], A[11], A[12], A[13], A[14], A[15]};
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+
+            ALU_SRL : begin
+                gold_out = A >> B[3:0];
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+            ALU_SLL : begin
+                gold_out = A << B[3:0];
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+            ALU_ROR : begin
+                gold_out = A >> B[3:0] | A << (16-B[3:0]);
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+            ALU_ROL : begin
+                gold_out = A << B[3:0] | A >> (16-B[3:0]);
+                if (gold_out[15:0] !== out) cerror = 1'b1;
+            end
+        endcase
+
+        if (cerror === 1'b1) begin
+            $display("ERRORCHECK :: ALU :: Inputs :: op = %b , A = %x, B = %x :: Outputs :: out = %x, carryout = %x :: Expected :: out = %x, carryout = %x",
+                op, A, B,
+                out, carryout,
+                gold_out[15:0], gold_carryout);
+            fail = 1;
+        end
+    end
+endmodule
