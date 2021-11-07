b9194b2f955b7dda4ea35661fca8f390f91c6a98
Erin Marshall <limarshall@wisc.edu>
2021-11-07 15:51:48 -0600

    refactor FCU to use defs

--- proj_unpipelined/fcu.v
+++ proj_unpipelined/fcu.v
@@ -1,9 +1,9 @@
 module fcu( // Flag Computation Unit
-	input wire [15:0] alu_out,
-	input wire alu_carry,
-	input wire [2:0] op,
+    input wire [15:0] alu_out,
+    input wire alu_carry,
+    input wire [2:0] op,
 
-	output reg flag
+    output reg flag
 );
     // zero bit computation
     wire zero;
@@ -14,24 +14,25 @@ module fcu( // Flag Computation Unit
     assign sgn = alu_out[15];
 
 
+    `include "ops.vh"
     // op        function
     // ----------------------
-	// 000       A==B
-	// 001       A!=B
-	// 010       A<B
-	// 011       A>B
-	// 100       A<=B
-	// 101       A>=B
-	// 11x       carries out?
+    // 000       A==B
+    // 001       A!=B
+    // 010       A<B
+    // 011       A>B
+    // 100       A<=B
+    // 101       A>=B
+    // 11x       carries out?
 
     // everything except FLU_CRY assumes aluop = ALU_SUB
     always @* casex (op)
-    	3'b000 : flag = zero; // -A + B = 0
-    	3'b001 : flag = !zero;
-    	3'b010 : flag = !zero && !sgn; // not zero and -A + B positive => A < B
-    	3'b011 : flag = sgn; // -A + B negative, nonzero => A > B
-    	3'b100 : flag = !sgn; // -A + B positive or zero => A <= B
-    	3'b101 : flag = sgn | zero; // -A + B negative or zero => A >= B
-    	3'b11? : flag = alu_carry; // flag is set if we carry out
-	endcase
+        default : flag = zero; // FCU_EQ: -A + B = 0
+        FCU_NEQ : flag = !zero;
+        FCU_LT  : flag = !zero && !sgn; // not zero and -A + B positive => A < B
+        FCU_GT  : flag = sgn; // -A + B negative, nonzero => A > B
+        FCU_LE  : flag = !sgn; // -A + B positive or zero => A <= B
+        FCU_GE  : flag = sgn | zero; // -A + B negative or zero => A >= B
+        FCU_CRY : flag = alu_carry; // flag is set if we carry out
+    endcase
 endmodule
