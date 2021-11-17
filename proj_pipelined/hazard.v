88b42ab0df592d2d1aeef222592027e44e194144
Erin Marshall <limarshall@wisc.edu>
2021-11-16 19:42:17 -0600

    stub hazard unit

---
+++ proj_pipelined/hazard.v
@@ -0,0 +1,7 @@
+// hazard unit; checks current pipeline state for hazards
+// and either stalls appropriately or forwards needed data
+// by controlling forwarding muxes.
+module hazard (
+);
+
+endmodule
