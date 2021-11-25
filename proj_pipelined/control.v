461bf1c35970bf6c7bba094202c941661b42326d
Erin Marshall <limarshall@wisc.edu>
2021-11-24 22:21:15 -0600

    control: cleanup

--- proj_pipelined/control.v
+++ proj_pipelined/control.v
@@ -1,18 +1,3 @@
-/*module control(
-    input wire [4:0] opcode,
-    input wire [1:0] op_ext,
-
-    output wire instr_rformat,
-    output wire writeto_rs,
-    output wire readfrom_rd,
-    output wire link,
-
-    output wire err
-);
-    assign err = 1'b0;
-endmodule
-*/
-
 `default_nettype none
 module control(
     input wire [4:0] opcode,
